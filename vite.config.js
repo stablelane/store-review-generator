f714be6ba3fed21db2e81a0561751e4f18cdefee
stablelane <aaanshad09@gmail.com>
2024-12-12 21:03:07 +0530

    add breakpoint for mobile devices

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // This allows access from other devices on the same network
+    port: 5173, // This is the default port, but you can change it if needed
+  },
+});
